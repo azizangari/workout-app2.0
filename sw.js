326e5f355b3f8f00a73e2f61bed8077075151d2a
Aziz Alangari <34690848+azizangari@users.noreply.github.com>
2026-06-28 19:20:58 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* Rest timer + always fetch fresh HTML when online. Version: 2025.06.27-3 */
+/* Rest timer + always fetch fresh HTML when online. Version: 2025.06.27-4 */
 const REST_DONE_TAG = 'rest-done';
 const REST_TIMER_TAG = 'rest-timer';
 
